3483b32936227737d0a92775d0d83a66fed73524
Alex <alexmihgel@gmail.com>
2022-05-08 17:46:57 +0300

    add cocoa touch for ColorGame Screen

---
+++ Usov alex-rightToTarget/HexToColorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HexToColorViewController.swift
+//  Usov alex-rightToTarget
+//
+//  Created by Алексей Попроцкий on 08.05.2022.
+//
+
+import UIKit
+
+class HexToColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
